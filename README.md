1287622d51a61b6836b314b964d5ac71dc5d6d44
FANG <389094993@qq.com>
2018-08-14 21:36:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# WordPress 博客微信小程序
+
+### 预览
+
+![](https://moooc.oss-cn-shenzhen.aliyuncs.com/blog/wechatmp.png)
+
+### 说明
+
+在这个基础上修改而来
+
+https://github.com/iamxjb/winxin-app-watch-life.net
+
+1. 无需安装额外插件
+2. 目前还有一些 Bug
+
---
+++ utils/config.js
@@ -0,0 +1,49 @@
+/*
+ * 
+ * WordPres版微信小程序
+ * author: jianbo
+ * organization: 守望轩  www.watch-life.nets
+ * github:    https://github.com/iamxjb/winxin-app-watch-life.net
+ * 技术支持微信号：iamxjb
+ * 开源协议：MIT
+ * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
+ */
+
+
+
+//配置域名,域名只修改此处。
+//如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
+var DOMAIN = "moooc.cc";
+var MINAPPTYPE="1";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
+var WEBSITENAME="FANG"; //网站名称
+var ABOUTID = 2; //wordpress网站"页面"的id,注意这个"页面"是wordpress的"页面"，不是"文章"
+var PAGECOUNT='10'; //每页文章数目
+var CATEGORIESID='all'  //专题页显示全部的分类
+//var CATEGORIESID = '1,1059,98,416,189,374,6,463';//指定专题页显示的分类的id
+var INDEXLISTTYPE="all" //首页显示所有分类
+//var INDEXLISTTYPE ="1" //指定首页显示分类的id
+var PAYTEMPPLATEID = 'hzKpxuPF2rw7O-qTElkeoE0lMwr0O4t9PJkLyt6v8rk';//赞赏消息模版id
+var REPLAYTEMPPLATEID = 'IiAVoBWP34u1uwt801rI_Crgen7Xl2lvAGP67ofJLo8';//回复评论消息模版id
+var ZANIMAGEURL = 'https://moooc.cc/wp-content/themes/bigsize/wechat.png';//微信赞赏的图片链接，用于个人小程序的赞赏
+var LOGO = "../../images/logo-icon.png"; // 网站的logo图片
+var POSTERIMGURL ="../../default-banner.jpg"; //生成海报如果没有首图，使用此处设置的图片作为海报图片。
+//设置downloadFile合法域名,不带https ,在中括号([])里增加域名，格式：{id=**,domain:'www.**.com'}，用英文逗号分隔。
+//此处设置的域名和小程序与小程序后台设置的downloadFile合法域名要一致。
+var DOWNLOADFILEDOMAIN = [
+  { id: 1, domain: 'moooc.oss-cn-shenzhen.aliyuncs.com' }
+]
+export default {
+  getDomain: DOMAIN,
+  getWebsiteName: WEBSITENAME,
+  getAboutId: ABOUTID,
+  getPayTemplateId: PAYTEMPPLATEID,
+  getPageCount: PAGECOUNT,
+  getCategoriesID :CATEGORIESID,
+  getReplayTemplateId: REPLAYTEMPPLATEID,
+  getMinAppType: MINAPPTYPE,
+  getZanImageUrl: ZANIMAGEURL,
+  getIndexListType: INDEXLISTTYPE,
+  getLogo: LOGO,
+  getPostImageUrl: POSTERIMGURL,
+  getDownloadFileDomain: DOWNLOADFILEDOMAIN
+}
